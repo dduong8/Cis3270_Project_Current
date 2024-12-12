424facb36628136c9fc994759f4ecf1e724b86c3
davidduong <dduong8@student.gsu.edu>
2024-12-12 02:23:12 -0500

    David new push test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.cis3270_project_current {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.cis3270_project_current to javafx.fxml;
+    exports org.example.cis3270_project_current;
+}
